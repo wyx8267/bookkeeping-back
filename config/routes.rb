cf89e6b0212b23f3d426683a24c73e692557b710
NEKOI <wyx8267@foxmail.com>
2022-08-21 15:56:11 +0800

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
