8e8cb8160cc3e2fd2c02cd521723e4113b4d6e5f
NEKOI <wyx8267@foxmail.com>
2022-09-04 21:51:24 +0800

    添加根路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get '/', to: "home#index"
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
