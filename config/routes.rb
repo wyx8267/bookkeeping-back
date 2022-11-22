b138223b01b04f3a34b98efe34613c8c31430bd6
NEKOI <wyx8267@foxmail.com>
2022-11-22 21:46:29 +0800

    创建jwt

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
-      resources :session, only: [:create, :destroy]
+      resources :sessions, path: 'session', only: [:create, :destroy]
       resources :me, only: [:show]
       resources :items
       resources :tags
