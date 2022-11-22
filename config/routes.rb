554163a3c1a28d60eb3882f37cc3a62494c137b2
NEKOI <wyx8267@foxmail.com>
2022-11-22 22:34:22 +0800

    完成me接口，路由修复

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,8 +6,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :validation_codes, only: [:create]
-      resources :sessions, path: 'session', only: [:create, :destroy]
-      resources :me, only: [:show]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
       resources :items
       resources :tags
     end
