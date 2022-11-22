86b12e0839d3db5a4102f51979922865918cde8c
NEKOI <wyx8267@foxmail.com>
2022-11-22 15:52:19 +0800

    使用enum, 测试发送频繁

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
+    if ValidationCode.exists?(email: params[:email], kind: "sign_in", created_at: 1.minute.ago..Time.now)
+      render status: :too_many_requests
+      return
+    end
+    # 语法糖版↓
+    # return render status: :too_many_requests if ValidationCode.exists?(email: params[:email], kind: "sign_in", created_at: 1.minute.ago..Time.now)
+
     validation_code = ValidationCode.new email: params[:email], kind: "sign_in"
     if validation_code.save
       render status: 200
