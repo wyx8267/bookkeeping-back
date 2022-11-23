a06e9f2b0ffa644a64f0fc10e8e6ada1ef275cde
NEKOI <wyx8267@foxmail.com>
2022-11-23 23:13:16 +0800

    获取单个标签api及文档

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,51 +1,60 @@
 class Api::V1::TagsController < ApplicationController
   def index
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return render status: 401 if current_user_id.nil?
-    tags = Tag.where({user_id: current_user_id}).page(params[:page])
+    tags = Tag.where({ user_id: current_user_id }).page(params[:page])
     render json: { resources: tags, pager: {
       page: params[:page] || 1,
       per_page: Tag.default_per_page,
-      count: Tag.count
-    }}
+      count: Tag.count,
+    } }
   end
 
   def create
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return render status: 401 if current_user_id.nil?
 
     tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user_id
     if tag.save
-      render json: {resource: tag}, status: :ok
+      render json: { resource: tag }, status: :ok
     else
-      render json: {errors: tag.errors}, status: :unprocessable_entity
+      render json: { errors: tag.errors }, status: :unprocessable_entity
     end
   end
 
   def update
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return render status: 401 if current_user_id.nil?
 
     tag = Tag.find params[:id]
     tag.update params.permit(:name, :sign)
     if tag.errors.empty?
-      render json: {resource: tag}, status: :ok
+      render json: { resource: tag }, status: :ok
     else
-      render json: {errors: tag.errors}, status: :unprocessable_entity
+      render json: { errors: tag.errors }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return render status: 401 if current_user_id.nil?
-    
+
     tag = Tag.find params[:id]
-    return head :forbidden unless tag.user_id === current_user_id
+    return head :forbidden unless tag.user_id == current_user_id
     tag.deleted_at = Time.now
     if tag.save
       head 200
     else
-      render json: {errors: tag.errors}, status: :unprocessable_entity
+      render json: { errors: tag.errors }, status: :unprocessable_entity
     end
+  end
+
+  def show
+    current_user_id = request.env["current_user_id"]
+    return render status: 401 if current_user_id.nil?
+
+    tag = Tag.find params[:id]
+    return head :forbidden unless tag.user_id == current_user_id
+    render json: { resource: tag }
   end
 end
