539905c40729b98425ec93adc979eaa6c76ec62c
NEKOI <wyx8267@foxmail.com>
2022-11-23 15:09:30 +0800

    items 接口及文档

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page])
+    items = Item.where({created_at: params[:created_after]..params[:created_before]})
+      .page(params[:page])
     render json: {resources: items, pager: {
-      page: params[:page],
-      per_page: 10,
+      page: params[:page] || 1,
+      per_page: Item.default_per_page,
       count: Item.count
     }}
   end
