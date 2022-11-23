294997451e34cb81b6591c2ad9e28d6c93425e13
NEKOI <wyx8267@foxmail.com>
2022-11-23 21:38:13 +0800

    创建标签api

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,12 +1,23 @@
 class Api::V1::TagsController < ApplicationController
   def index
     current_user_id = request.env['current_user_id']
-    return head :not_found if current_user_id.nil?
+    return render status: 401 if current_user_id.nil?
     tags = Tag.where({user_id: current_user_id}).page(params[:page])
     render json: { resources: tags, pager: {
       page: params[:page] || 1,
       per_page: Tag.default_per_page,
       count: Tag.count
     }}
+  end
+  def create
+    current_user_id = request.env['current_user_id']
+    return render status: 401 if current_user_id.nil?
+
+    tag = Tag.new name: params[:name], sign: params[:sign], user_id: current_user_id
+    if tag.save
+      render json: {resource: tag}, status: :ok
+    else
+      render json: {errors: tag.errors}, status: :unprocessable_entity
+    end
   end
 end
